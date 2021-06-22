203450324b8968c4261bf3f116d0d499c23f3f63
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2021-06-22 23:50:13 +0100

    Initial commit

---
+++ lang/en/local_ehl.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for local_ehl
+ *
+ * @package   local_ehl
+ * @copyright 2021 Ecole hôtelière de Lausanne {@link https://www.ehl.edu/}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'EHL Moodle customisation';
